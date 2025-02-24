15e85a5664bdc8d5559dd1d21c9980f31e14c3b6
kenji0302 <kenji0302@gmail.com>
2025-02-24 14:07:16 +0900

    fontサイズ修正

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from cal import jst_ymd, wifi_connect, refresh_access_token, jpredtext, jpblackt
 from Pico_ePaper_5_83_B import EPD_5in83_B
 from mfont import mfont
 from secret import WIFI_SSID, WIFI_PASSWORD,GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN,GOOGLE_CALENDAR_ID
-import gc
 
 if __name__=='__main__':
     
@@ -42,32 +41,26 @@ if __name__=='__main__':
             jpredtext("トークンを更新してください。", 5, 35, mf, epd)
         else:
 
-            print(gc.mem_free())
-
             calendar_id = "primary"
 
             ymdhms = jst_today_ymdhms_for_api() 
             
-            url = "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events?maxResults=15&timeMin=" + ymdhms + "&orderBy=startTime&singleEvents=True&timeZone=JST"
+            url = "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events?maxResults=13&timeMin=" + ymdhms + "&orderBy=startTime&singleEvents=True&timeZone=JST"
 
             headers = {
                 "Authorization": "Bearer " + access_token
             }
-            print(gc.mem_free())
+
             response = urequests.get(url, headers=headers)
-            print(gc.mem_free())
+
             if response.status_code == 200:
 
                 events = response.json().get('items', [])            
                 
-                mf = mfont()
-                mf.setFontSize(16)    
-
                 x = 10
                 i = 0
                 
                 for event in events:
-                    print(gc.mem_free())
                     if 'date' in event['start']:
                         event_ymd = event['start']['date'][0:4] + event['start']['date'][5:7] + event['start']['date'][8:10]
                         event_str = event['start']['date'][5:7] + '月' + event['start']['date'][8:10] + '日' + ' ' + event['summary']
@@ -81,14 +74,10 @@ if __name__=='__main__':
                         jpredtext(event_str, 5, x, mf, epd)
                     else:
                         jpblacktext(event_str, 5, x, mf, epd)
-                    x = x + 30
-                    # i = i + 1
-                    # if i % 2 == 0:
-                        # epd.display(epd.buffer_black, epd.buffer_red)
+                    x = x + 37
             else:
                 print("Failed to retrieve events:", response.status_code, response.text)
 
-        print(gc.mem_free())
         epd.display(epd.buffer_black, epd.buffer_red)
         
         print("deepsleep")
